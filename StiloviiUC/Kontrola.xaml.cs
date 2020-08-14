fc3d56c97be106409202f9082e6223b73cf8d59e
Dragan Pepic <d.pepic@hotmail.com>
2020-08-14 17:31:33 +0200

    Add project files.

---
+++ StiloviiUC/Kontrola.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StiloviiUC
+{
+	/// <summary>
+	/// Interaction logic for Kontrola.xaml
+	/// </summary>
+	public partial class Kontrola : UserControl
+	{
+		public string Nesto { get; set; }
+		public Kontrola()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+	}
+}
